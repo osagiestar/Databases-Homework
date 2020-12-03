8a050a1df00c8e79313471dffc7978fca94a9dda
osagiestar <okoedooc@yahoo.com>
2020-12-03 06:38:43 +0000

    query products using LIKE

--- week-3/mandatory/2-api/cyf-ecommerce-api/server.js
+++ week-3/mandatory/2-api/cyf-ecommerce-api/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded());
+// app.use(express.urlencoded());
 require('dotenv').config();
 
 const { Pool } = require("pg");
@@ -14,50 +14,25 @@ const pool = new Pool({
   port: 5432,
 });
  
-// app.get("/products", function (req, res) {
-//   pool.query("SELECT * FROM products", (error, result) => {
-//     res.json(result.rows);
-//   });
-// });
+app.get("/products", function (req, res) {
+  let query = `SELECT * FROM products ORDER BY product_name`;
+  pool.query(query)
+  .then((result) => res.status(200).json(result.rows))
+  .catch((e) => console.error(e));
+}); 
 
 app.get("/products", function (req, res) {
-  const queryList = req.query.name;
-  let query = `SELECT * FROM products ORDER BY product_name`;
-
-  if(queryList) {
-    query = `SELECT * FROM products WHERE product_name LIKE '$%{queryList}%'`;
-  }
-  pool.query(query)
-  .then((result) => res.json(result.rows))
-    .catch((e) => console.error(e));
+  let queryList = req.query.name;
+  let query = "SELECT * FROM products";
+    if(queryList) {
+      query = `SELECT * FROM products WHERE product_name LIKE '%${queryList}%'`;
+    };
+    pool
+      .query(query)
+      .then((result) => res.json(result.rows))
+      .catch((e) => console.error(e));
 }); 
 
-app.get("/customers/:id", function (req, res) {
-  const customerId = req.params.id;
-  
-  pool
-    .query("SELECT * FROM customers WHERE id = $1", [customerId])
-    .then((result) => res.json(result.rows))
-    .catch((e) => console.error(e));
-}); 
- 
-app.post("/customers", (req, res) => {
-  const newCustomerName = req.body.name;
-  const newCustomerAddress = req.body.address;
-  const newCustomerCity = req.body.city;
-  const newCustomerCountry = req.body.country;
-
-  const query =
-    "INSERT INTO customers (name, address, city, country) VALUES ('Mario Balotelli', '89 Pizza Street', 'Milan', 'Italy')";
-    if (typeof newCustomerName !== "string") {
-    return res.status(400).send("Customer's name must be words or alphabets");
-  }
-  pool
-    .query(query, [newCustomerName, newCustomerAddress, newCustomerCity, newCustomerCountry])
-    .then(() => res.send("New customer created!"))
-    .catch((e) => console.error(e));
-});
-
-app.listen(3000, function () {
-  console.log("Server is listening on port 3000. Ready to accept requests!");
+app.listen(3006, function () {
+  console.log("Server is listening on port 3006. Ready to accept requests!");
 });
